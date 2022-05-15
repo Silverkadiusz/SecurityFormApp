40ea3d37f1b31562f0718eecc0d6a9c9ecab828e
Silverkadiusz55455 <arkadiusz55455@gmail.com>
2022-05-15 12:38:30 +0200

    v1

---
+++ src/test/java/com/example/securityformapplication/SecurityFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityformapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
